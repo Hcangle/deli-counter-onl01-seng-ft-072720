eb1103a62013e8e8180697d641983d68fbe1b951
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 05:14:50 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,12 @@
+def line(katz_deli)
+  katz_line = []
+  katz_string = "The line is currently:"
+  if katz_deli.length > 0
+    katz_deli.each_with_index do |name, num|
+    katz_string << (" #{num+1}. #{name}")
+  end
+    puts katz_string
+  else
+    puts "The line is currently empty."
+  end
+end
