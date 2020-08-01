c09cb161b686243a0b1c1499fda9f8aba1cb7df2
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 05:11:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+  katz_line = []
+  katz_string = "The line is currently: "
+  if katz_deli.length > 0
+    katz_deli.each_with_index do |name, num|
+    katz_line.push("#{num+1}. #{name}")
+    end
+    katz_string = katz_string+katz_line.join(" ")
+    puts katz_string
+  else
+    puts "The line is currently empty."
+  end
+end
